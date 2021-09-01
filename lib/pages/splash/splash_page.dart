6f372e28d0d07601eb606b3846a660a8e1d719f7
Alvaro Prado <alvarob.prado@hotmail.com>
2021-09-01 18:24:16 -0400

    create splash page

--- lib/pages/splash/splash_page.dart
+++ lib/pages/splash/splash_page.dart
@@ -4,5 +4,25 @@ class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Scaffold();
+  Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 2));
+    return Scaffold(
+      backgroundColor: const Color(0xff05820A),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/logo.png',
+              height: 250,
+            ),
+            const SizedBox(height: 20),
+            CircularProgressIndicator(
+              color: Colors.green.shade900,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
